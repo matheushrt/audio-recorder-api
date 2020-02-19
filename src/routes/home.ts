6617ae9ed56e54da690e246a50438e4d480b46a0
Matheus Souza <matheushrt@gmail.com>
2020-02-19 10:42:17 -0300

    updated home route to test local audio recording

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,9 +1,36 @@
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
 const router = express.Router();
+const audioFilesPath = path.resolve(__dirname, '../../audios');
+const storage = multer.diskStorage({
+  destination: audioFilesPath,
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}.mp3`);
+  }
+});
+const upload = multer({ storage });
+
+const getFileNames = (): string[] => fs.readdirSync(audioFilesPath);
+const getFilePath = (fileName: string): string =>
+  path.join(audioFilesPath, fileName);
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.send('All set!');
+router.get('/file/:fileName', (req, res) => {
+  const { fileName } = req.params;
+  const filePath = getFilePath(fileName);
+  res.status(200).sendFile(filePath, console.error);
+});
+
+router.get('/file_names', (req, res) => {
+  res.status(200).json(getFileNames());
+});
+
+//  POST
+router.post('/', upload.single('audio'), (req, res) => {
+  res.status(201).send('Audio file successfully saved!');
 });
 
 export default router;
